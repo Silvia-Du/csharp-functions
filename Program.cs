223a7bdd5feadfcd26dc656f1e42b28934565ea6
Silvia-Du <silviadu@hotmail.it>
2022-09-17 14:35:23 +0200

    Fine seconda funzione

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /*
- void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, 
-elemento 2, elemento 3, ...]“.
+ 1- void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array in questa forma “[elemento 1, 
+    elemento 2, elemento 3, ...]“.
 
-int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
+ 2- int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
 
 int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato. 
 Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione! Vi ricordate perchè? Pensateci (vedi slide)
@@ -17,6 +17,8 @@ Stampare la somma di tutti i numeri
 Stampare la somma di tutti i numeri elevati al quadrati
   */
 
+
+//1
 int[] numberCollection = { 1, 3, 5, 8, 7, 8, 15, 18 };
 
 void StampaArray(int[] array)
@@ -33,3 +35,14 @@ void StampaArray(int[] array)
 }
 
 StampaArray(numberCollection);
+
+//2
+int exampleNumber = 10;
+
+int Quadrato(int numero)
+{
+    return numero * numero;
+}
+
+int result = Quadrato(exampleNumber);
+Console.WriteLine(result);
